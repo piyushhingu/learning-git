64286fa03f1475631d3a2117f8070fc29ffb0d0e
Hingu piyush <81574814+piyushhingu@users.noreply.github.com>
2023-02-22 15:57:11 +0530

    Update hubconf.py

--- hubconf.py
+++ hubconf.py
@@ -16,21 +16,23 @@ from torch.utils.data import DataLoader
 from torchvision import datasets
 from torchvision.transforms import ToTensor
 
-# Download training data from open datasets.
-training_data = datasets.FashionMNIST(
+def data_load():
+  training_data = datasets.FashionMNIST(
     root="data",
     train=True,
     download=True,
     transform=ToTensor(),
-)
+  )
 
-# Download test data from open datasets.
-test_data = datasets.FashionMNIST(
+  # Download test data from open datasets.
+  training_data = datasets.FashionMNIST(
     root="data",
     train=False,
     download=True,
     transform=ToTensor(),
-)
+  )
+  return training_data,training_data;
+
 
 # # classes = [
 # #     "T-shirt/top",#0
